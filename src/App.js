6f499960c5a4a16184629e3e63a26c1e096533a3
David Fontaine <david.fontaine85@gmail.com>
2021-02-09 10:54:21 +0100

    trying another way to useEffect with async Axios 2nd attempt

--- src/App.js
+++ src/App.js
@@ -57,7 +57,11 @@ function App() {
         <div>
             <h2>Membres de l'équipage</h2>
             <section className="member-list">
-                {crewList.map((item)=>{<div key={item.id} className="member-item">{item.crew_membername}</div>})}
+                {crewList.map((item)=>{
+                  return (
+                    <div key={item.id} className="member-item">{item.crew_membername}</div>
+                  );
+                })}
             </section>
           </div>
       </main>
