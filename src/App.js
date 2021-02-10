c07dc100f781086d8bd242a9c57089103602d6c6
David Fontaine <david.fontaine85@gmail.com>
2021-02-10 06:02:39 +0100

    Add Comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import './App.css';
+import React, { useState, useEffect } from 'react';     //Using React Hooks
+import Axios from 'axios';                              //Using Axios to cleaning lisibility of Async Queries
+import Header from './Components/Header';               //Static Component for clean code
+import Footer from './Components/Footer';               //Static Component for clean code
+import './App.css';                                     //Import CSS thanks to Boss Jason (with some adjustments to set the three columns display)
 
 function App() {
 
+  //State declaration on str to insert and on Array to Select and display
   const [newMember, setNewMember] = useState('');
   const [crewList, setCrewList] = useState([]);
 
-  /*useEffect(()=>{
-    Axios.get("https://tech-challenge-wild-code.herokuapp.com/api/get").then((response)=>{
-      //setCrewList(response);
-      console.log(response.data);
-    });
-  }, []);*/
 
+  //Using Hooks to pass through class component and avoid ComponentDidMount and ComponentDidUpdate Method
+  //Fetch all crew_membername from crew_members table return in Array in const crewList
   useEffect(()=> {
     const fetchData = async () => {
       Axios.get('https://tech-challenge-wild-code.herokuapp.com/api/get').then((response)=> {
@@ -26,12 +23,16 @@ function App() {
     fetchData();
   }, []);
 
+
+  //Pass input Value to newMember which is send to backend by POST method from Axios
+  //And will be use as string to prepare query
   const submitNewMember = () =>{
     Axios.post("https://tech-challenge-wild-code.herokuapp.com/api/insert", {newMember: newMember}).then(()=>{
       alert("Nouveau membre ajouter à l'équipage!");
     });
   }
 
+  
   return (
     <div className="App">
       <Header />
